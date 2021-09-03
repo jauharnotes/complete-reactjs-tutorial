469ece73ec0e3d69d5269470a1fe995a659d0c1d
Jauharuddin <jauharnotes@gmail.com>
2021-09-03 23:19:26 +0700

    Function Paradigm

--- chapter-01/functional-paradigm.js
+++ chapter-01/functional-paradigm.js
@@ -38,3 +38,62 @@ const logCompLiment4 = function (firsName, message) {
 };
 
 logCompLiment4("jauhar", "You're so cool");
+// console.log(logCompLiment4("jauhar", "You're so cool"));
+
+// default value
+const defaultPerson = {
+  name: {
+    firsName: "jauhar",
+    lastName: "Uddin",
+  },
+  activity: "reading book",
+};
+
+function logaTivity(person = defaultPerson) {
+  console.log(
+    `hello nama saya ${person.name.firsName} ${person.name.lastName}, saya hobi ${person.activity}`
+  );
+}
+
+logaTivity();
+
+// arrow function
+const lordify = (firsName, lord) => `${firsName} of ${lord}`;
+console.log(lordify("jauhar", "depok"));
+
+// arrow function with multiple line
+const kota = (firsName, loard) => {
+  if (!firsName) {
+    throw new Error("A firsname requared to kota");
+  } else if (!loard) {
+    throw new Error("A loard requared to kota");
+  } else {
+    return `${firsName} of ${loard}`;
+  }
+};
+
+console.log(kota("jauhar", "depok"));
+// console.log(kota("jauhar")); // Error
+
+// function returing object
+const foo = (firsName, lastName) => ({
+  firs: firsName,
+  last: lastName,
+});
+
+console.log(foo("jauhar", "uddin"));
+
+// babel
+const add = (x = 5, y = 8) => console.log(x + y);
+
+add();
+
+let add2 = function add() {
+  let x =
+    arguments.length <= 0 || arguments[0] === undefined ? 5 : arguments[0];
+  let y =
+    arguments.length <= 1 || arguments[1] === undefined ? 8 : arguments[1];
+  return console.log(x + y);
+};
+
+add2();
